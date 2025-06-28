87e13af6c1fd8019e03fc9bf7582deeca74a8417
Daria Luhova <missis.lugowaya@gmail.com>
2025-06-29 01:45:01 +0200

    change export burger-menu.js

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,11 +1,15 @@
-const burgerBtn = document.querySelector('.menu-btn');
-const sidebar = document.querySelector('.sidebar');
-const closeBtn = document.querySelector('.close-btn');
-
-burgerBtn.addEventListener('click', () => {
-  sidebar.classList.add('active');
-});
-
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.remove('active');
-});
+export function initBurgerMenu() {
+  const burgerBtn = document.querySelector('.menu-btn');
+  const sidebar = document.querySelector('.sidebar');
+  const closeBtn = document.querySelector('.close-btn');
+  
+  if (!burgerBtn) return;
+  
+  burgerBtn.addEventListener('click', () => {
+    sidebar.classList.add('active');
+  });
+  
+  closeBtn.addEventListener('click', () => {
+    sidebar.classList.remove('active');
+  });
+}
