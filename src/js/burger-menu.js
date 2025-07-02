d00394c363695d8b4ab10b05066b21e007d32624
wrldworry <wrldworry@gmail.com>
2025-07-02 22:23:20 +0300

    favmergepls

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -2,30 +2,29 @@ export function initBurgerMenu() {
   const burgerBtn = document.querySelector('.menu-btn');
   const sidebar = document.querySelector('.sidebar');
   const closeBtn = document.querySelector('.close-btn');
-  const overlay = document.querySelector('.modal-overlay-brg');
+  const overlay = document.querySelector('.modal-overlay');
 
   if (!burgerBtn) return;
 
   const openMenu = () => {
     sidebar.classList.add('active');
-    document.body.classList.add('sidebar-open');
+    document.body.classList.add('no-scroll');
+    overlay.classList.add('is-open');
   };
 
   const closeMenu = () => {
     sidebar.classList.remove('active');
-    document.body.classList.remove('sidebar-open');
+    document.body.classList.remove('no-scroll');
+    overlay.classList.remove('is-open');
   };
 
   burgerBtn.addEventListener('click', openMenu);
   closeBtn.addEventListener('click', closeMenu);
-  document.addEventListener('click', event => {
-    const isClickInside =
-      sidebar.contains(event.target) || burgerBtn.contains(event.target);
-    if (!isClickInside) {
-      document.body.classList.remove('sidebar-open');
-      sidebar.classList.remove('active');
-      document.body.classList.remove('no-scroll');
-      overlay.classList.remove('is-open');
-    }
+  overlay.addEventListener('click', closeMenu);
+
+  sidebar.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
   });
 }
